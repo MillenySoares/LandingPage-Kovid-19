75a3e17ede97e08b485e62a05c8aae9ad679a3e7
MillenySoares <moreiramilleny8@gmail.com>
2021-09-24 13:28:54 -0300

    Sem Jquery

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var nav = $('.topo');
-
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 136) {
-        nav.addClass("fixar");
-    } else {
-        nav.removeClass("fixar");
-    }
-});
