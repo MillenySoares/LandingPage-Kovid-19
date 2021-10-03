273d729ff3f1772e60b8f0c2816ae889f9006fb9
MillenySoares <moreiramilleny8@gmail.com>
2021-10-02 23:19:11 -0300

    Menu com responsividade

---
+++ js/app.js
@@ -0,0 +1,34 @@
+var menu_aberto_aberto = 0;
+
+$('#bottun_abrir').on('click', function(e){
+    
+	if(menu_aberto_aberto == 0){
+		abrirMenu();
+	}else{
+		fecharMenu();
+	}
+	
+});
+$('#button_fechar').on('click', function(e){
+    
+	if(menu_aberto_aberto == 0){
+		abrirMenu();
+	}else{
+		fecharMenu();
+	}
+	
+});
+
+function abrirMenu(){
+	$('#bg_menu_mobile').show(500);
+	$('#bottun_abrir').addClass('botao-menu-mobile-ativo');
+	menu_aberto_aberto = 1;
+}
+
+function fecharMenu(){
+	$('#bg_menu_mobile').hide(500);
+	$('#button_fechar').removeClass('botao-menu-mobile-ativo');
+	menu_aberto_aberto = 0;
+}
+
+
