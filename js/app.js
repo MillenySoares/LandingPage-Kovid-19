487fd31777eb113d9c17b6c258a9219c9dad852d
MillenySoares <moreiramilleny8@gmail.com>
2021-10-05 16:46:30 -0300

    Quarta section finalizada, sem responsividade

--- js/app.js
+++ js/app.js
@@ -31,4 +31,27 @@ function fecharMenu(){
 	menu_aberto_aberto = 0;
 }
 
+$(document).ready(function(){
+
+	$(".counter").each(function() {
+	  var $this = $(this),
+  
+		countTo = $this.attr("data-count");
+  
+	  $({ countNum: $this.text()}).animate({
+		countNum: countTo
+	  },
+  
+	  	{
+			duration: 3000,
+			easing:"linear",
+			step: function() {
+			  $this.text(Math.floor(this.countNum));
+			},
+			complete: function() {
+			  $this.text(this.countNum);
+			}
+		});
+	});
+});
 
